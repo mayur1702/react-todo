5b0227c18c27389392689d7f96cb60498342e13e
mayur1702 <m.mistry17@yahoo.com>
2021-07-15 01:31:47 +0530

    added eslint and prettier dependencies configured eslint and prettier installed redux and react-redux dependencies implemented basic redux reducer, action and store

--- README.md
+++
@@ -1,2 +0,0 @@
-### CREATE REACT APP
-
--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReduxExample from './redux/ReduxExample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <ReduxExample />;
+};
 
 export default App;
